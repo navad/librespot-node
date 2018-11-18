65ad54cebbbd35aa0d9d8f185b1a22c2d2ad42e9
Nadav Tau <nadavt@gmail.com>
2018-11-18 21:47:15 +0200

    Adding basic web token support

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -10,6 +10,7 @@ extern crate simple_logging;
 
 use log::{ LevelFilter };
 
+use neon::context::Context;
 use neon::prelude::*;
 
 mod lib {
@@ -20,6 +21,52 @@ use lib::player::SpotifyPlayer;
 
 pub struct Spotify {
     player: SpotifyPlayer
+}
+
+pub struct AccessToken {
+    token: String,
+    scope: Vec<String>,
+    expires_in: u32,
+}
+
+declare_types! {
+    pub class JsAccessToken for AccessToken {
+        init(mut cx) {
+            let token: Handle<JsString> = cx.argument::<JsString>(0)?;
+            let expires_in: Handle<JsNumber> = cx.argument::<JsNumber>(1)?;
+
+            Ok(AccessToken {
+                token: token.value(),
+                scope: vec![ ],
+                expires_in: expires_in.value() as u32,
+            })
+        }
+
+        method getToken(mut cx) {
+            let this = cx.this();
+            let val = {
+                let guard = cx.lock();
+                let instance = this.borrow(&guard);
+
+                instance.token.clone()
+            };
+
+            Ok(cx.string(val).upcast())
+        }
+
+        method getExpiry(mut cx) {
+            let this = cx.this();
+            let val = {
+                let guard = cx.lock();
+                let instance = this.borrow(&guard);
+
+                instance.expires_in.clone()
+            };
+
+            Ok(cx.number(val).upcast())
+        }
+
+    }
 }
 
 declare_types! {
@@ -42,7 +89,7 @@ declare_types! {
             {
                 let guard = cx.lock();
                 let mut spotify = this.borrow_mut(&guard);
-                
+
                 spotify.player.play(track_id.value());
             }
 
@@ -50,12 +97,12 @@ declare_types! {
         }
 
         method stop(mut cx) {
-            let this = cx.this();   
+            let this = cx.this();
 
             {
                 let guard = cx.lock();
                 let spotify = this.borrow(&guard);
-                
+
                 spotify.player.stop();
             }
 
@@ -63,12 +110,12 @@ declare_types! {
         }
 
         method pause(mut cx) {
-            let this = cx.this();   
+            let this = cx.this();
 
             {
                 let guard = cx.lock();
                 let spotify = this.borrow(&guard);
-                
+
                 spotify.player.pause();
             }
 
@@ -77,12 +124,12 @@ declare_types! {
 
         method seek(mut cx) {
             let this = cx.this();
-            let position_ms: Handle<JsNumber> = cx.argument::<JsNumber>(0)?; 
+            let position_ms: Handle<JsNumber> = cx.argument::<JsNumber>(0)?;
 
             {
                 let guard = cx.lock();
                 let spotify = this.borrow(&guard);
-                
+
                 spotify.player.seek(position_ms.value() as u32);
             }
 
@@ -90,16 +137,60 @@ declare_types! {
         }
 
         method is_playing(mut cx) {
-            let this = cx.this();   
+            let this = cx.this();
 
             let is_playing = {
                 let guard = cx.lock();
                 let spotify = this.borrow(&guard);
-                
+
                 spotify.player.is_playing()
             };
 
             Ok(cx.boolean(is_playing).upcast())
+        }
+
+        method get_token(mut cx) {
+            let this = cx.this();
+            let ctor = JsAccessToken::constructor(&mut cx)?;
+
+            let client_id: Handle<JsString> = cx.argument::<JsString>(0)?;
+            let scopes: Handle<JsString> = cx.argument::<JsString>(1)?;
+            let cb: Handle<JsFunction> = cx.argument::<JsFunction>(2)?;
+
+            let mut token: Option<AccessToken> = None;
+
+            {
+                let guard = cx.lock();
+                let spotify = this.borrow(&guard);
+
+                spotify.player.get_token(
+                    client_id.value(),
+                    scopes.value(),
+                    |tok| {
+                        token = Some(AccessToken {
+                            token: tok.access_token,
+                            scope: vec![ ],
+                            expires_in: tok.expires_in
+                        });
+                    });
+            }
+
+            let tok = token.unwrap();
+
+            let args: Vec<Handle<JsValue>> = vec![
+                cx.string(tok.token).upcast(),
+                cx.number(tok.expires_in).upcast()
+            ];
+
+            let access_token_instance = ctor.construct(&mut cx, args);
+
+            let cb_args: Vec<Handle<JsValue>> = vec![
+                access_token_instance.unwrap().upcast(),
+            ];
+
+            let _ = cb.call(&mut cx, JsNull::new(), cb_args);
+
+            Ok(cx.undefined().upcast())
         }
     }
 }
--- native/src/lib/player.rs
+++ native/src/lib/player.rs
@@ -1,6 +1,6 @@
 use tokio_core::reactor::{ Core, Remote };
 
-use futures::{ Future, future };
+use futures::{ Future, Stream, future };
 use futures::sync::oneshot;
 use std::{ thread };
 use std::sync::{ Mutex, Arc };
@@ -10,9 +10,12 @@ use librespot::core::config::SessionConfig;
 use librespot::core::session::Session;
 
 use librespot::core::spotify_id::SpotifyId;
+use librespot::core::keymaster;
+use librespot::core::keymaster::Token;
 use librespot::playback::config::PlayerConfig;
 use librespot::playback::audio_backend;
 use librespot::playback::player::Player;
+use librespot::playback::player::PlayerEvent;
 
 pub struct PlayerState {
     is_playing: bool,
@@ -21,6 +24,7 @@ pub struct PlayerState {
 pub struct SpotifyPlayer {
     remote: Remote,
     player: Player,
+    session: Session,
     state: Arc<Mutex<PlayerState>>
 }
 
@@ -30,7 +34,7 @@ impl SpotifyPlayer {
         let (remote_tx, remote_rx) = oneshot::channel();
 
         let credentials = Credentials::with_password(username, password);
-        
+
         thread::spawn(move || {
             let mut core = Core::new().unwrap();
 
@@ -55,14 +59,35 @@ impl SpotifyPlayer {
 
         let backend = audio_backend::find(None).unwrap();
         let player_config = PlayerConfig::default();
-        let (player, _) = Player::new(player_config, session.clone(), None, move || (backend)(None));
+        let (player, rx) = Player::new(player_config, session.clone(), None, move || (backend)(None));
+
+        let state = Arc::new(Mutex::new(PlayerState {
+            is_playing: false
+        }));
+
+        let local_state = state.clone();
+
+        remote.spawn(move |_| {
+            rx.for_each(move |res| {
+                debug!("PlayerEvent: {:?}", res);
+
+                let mut state = local_state.lock().unwrap();
+
+                match res {
+                    PlayerEvent::Started { .. } => state.is_playing = true,
+                    PlayerEvent::Changed { .. } => debug!("Player state changed"),
+                    PlayerEvent::Stopped { .. } => state.is_playing = false,
+                }
+
+                Ok(())
+            })
+        });
 
         SpotifyPlayer {
             remote: remote,
             player: player,
-            state: Arc::new(Mutex::new(PlayerState {
-                is_playing: false
-            }))
+            session: session,
+            state: state,
         }
     }
 
@@ -73,8 +98,6 @@ impl SpotifyPlayer {
 
         let end_of_track = self.player.load(track, true, 0);
         let local_state = self.state.clone();
-
-        self.state.lock().unwrap().is_playing = true;        
 
         self.remote.spawn(move |_| {
             end_of_track.then(move |_result| {
@@ -100,5 +123,28 @@ impl SpotifyPlayer {
 
     pub fn is_playing(&self) -> bool {
         self.state.lock().unwrap().is_playing
+    }
+
+    pub fn get_token<F>(&self, client_id: String, scopes: String, cb: F)
+        where F: FnOnce(Token) {
+
+        let local_session = self.session.clone();
+        let (token_tx, token_rx) = oneshot::channel();
+
+        self.remote.spawn(move |_| {
+            keymaster::get_token(&local_session, &client_id, &scopes).then(move |res| {
+                let _ = token_tx.send(res);
+                Ok(())
+            })
+        });
+
+        match token_rx.wait().unwrap() {
+            Ok(r) => {
+                cb(r);
+            },
+            Err(e) => {
+                error!("Cannot get token {:?}", e);
+            }
+        };
     }
 }
